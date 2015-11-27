45f9c8ac2ff55f59d1777679b3e8f13ae61211b7
Alex Zhang <zhangdi_me@163.com>
2015-11-27 23:37:19 +0800

    添加 阿里云OSS

---
+++ src/AliyunStorage.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @author Di.Zhang <zhangdi_me@163.com>
+ */
+
+namespace yiizh\storage;
+
+
+use OSS\Core\OssException;
+use OSS\OssClient;
+use yii\base\Component;
+use yii\log\Logger;
+
+class AliyunStorage extends Component implements StorageInterface
+{
+    /**
+     * @var string
+     */
+    public $accessKey;
+
+    /**
+     * @var string
+     */
+    public $accessSecret;
+
+    /**
+     * @var string
+     */
+    public $endpoint;
+
+    /**
+     * @var bool
+     */
+    public $isCName = false;
+
+    /**
+     * @var string default null
+     */
+    public $securityToken = null;
+
+    /**
+     * @var string
+     */
+    public $bucket;
+
+    private $_client;
+
+    /**
+     * @inheritDoc
+     */
+    public function save($file, $content)
+    {
+        try {
+            $this->getClient()->putObject($this->bucket, $file, $content);
+            return true;
+        } catch (OssException $e) {
+            \Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR);
+            return false;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function read($file)
+    {
+        return $this->getClient()->getObject($this->bucket, $file);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function exists($file)
+    {
+        return $this->getClient()->doesObjectExist($this->bucket, $file);
+    }
+
+    /**
+     * @return OssClient
+     */
+    public function getClient()
+    {
+        if ($this->_client == null) {
+            $this->_client = new OssClient(
+                $this->accessKey,
+                $this->accessSecret,
+                $this->endpoint,
+                $this->isCName,
+                $this->securityToken
+            );
+        }
+        return $this->_client;
+    }
+
+}
